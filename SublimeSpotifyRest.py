393b81b12f3a2b96ab2bcbb746eb413ab5c3eb2e
Vladyslav Pekker <agilesteel@gmail.com>
2017-07-16 14:06:31 +0200

    Added settings.

--- SublimeSpotifyRest.py
+++ SublimeSpotifyRest.py
@@ -1,1 +1,13 @@
-print("Hello World!")
+import sublime
+import sublime_plugin
+
+from .src.sublime.SettingsManager import SettingsManager as SublimeSettingsManager
+
+sublime_settings_manager = SublimeSettingsManager("SublimeSpotifyRest.sublime-settings")
+
+def plugin_loaded():
+  sublime.active_window().run_command("show_panel", {"panel": "console", "toggle": True})
+
+class Sublime_spotify_rest_settingsCommand(sublime_plugin.ApplicationCommand):
+  def run(self):
+    sublime_settings_manager.open_settings_window()
---
+++ src/sublime/SettingsManager.py
@@ -0,0 +1,42 @@
+import sublime
+
+class SettingsManager:
+  def __init__(self, settings_file_name):
+    self.__settings_file_name = settings_file_name
+
+  def read_setting(self, key):
+    return self.__load_settings().get(key)
+
+  def set_setting(self, key, value):
+    self.__load_settings().set(key, value)
+
+  def write_setting(self, key, value):
+    self.set_setting(key, value)
+    self.__save_settings()
+
+  def __load_settings(self):
+    return sublime.load_settings(self.__settings_file_name)
+
+  def __save_settings(self):
+    sublime.save_settings(self.__settings_file_name)
+
+  def open_settings_window(self):
+    sublime.run_command("new_window")
+
+    settings_window = sublime.active_window()
+
+    left_right_group = {
+      "cols": [0.0, 0.5, 1.0], 
+      "rows": [0.0, 1.0], 
+      "cells": [[0, 0, 1, 1], [1, 0, 2, 1]]
+    }
+
+    settings_window.set_layout(left_right_group) # the left group is focused by default
+
+    packages = settings_window.extract_variables()["packages"]
+
+    settings_window.open_file("{}/sublime-spotify-rest/{}".format(packages, self.__settings_file_name))
+
+    settings_window.focus_group(1) # focus the right group
+
+    settings_window.open_file("{}/User/{}".format(packages, self.__settings_file_name))
