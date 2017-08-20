250e1626722c4ba20e7b40fb9ee5c640973c3eba
Vladyslav Pekker <agilesteel@gmail.com>
2017-08-20 10:16:21 +0200

    Video 8 - Trying out the absolute path again for the token.

--- src/spotify/Client.py
+++ src/spotify/Client.py
@@ -16,9 +16,11 @@ class Client:
       client_secret,
       redirect_uri = "http://localhost:" + str(redirect_port),
       scope = "user-read-currently-playing",
-      cache_path = os.path.join(
-        os.path.dirname(__file__),
-        "../../.cached-token"
+      cache_path = os.path.abspath(
+        os.path.join(
+          os.path.dirname(__file__),
+          "../../.cached-token"
+        )
       )
     )
 
